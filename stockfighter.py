8fcd59d5f420e3a449c7eef82b12962ca1fd9e98
RyanK1NY <ryank1ny@gmail.com>
2016-02-19 16:43:29 -0500

    Update with Order
    Able to do orders.

--- stockfighter.py
+++ stockfighter.py
@@ -3,12 +3,38 @@ import requests
 
 #https://www.stockfighter.io/ui/api_keys
 api_key = ""
-venue = "BJBEX"
-stock = "ZEYF"
+
+#On level page
+account = ""
+venue = "VWTEX"
+stock = "CUUM"
+
 
 API_HOST = "https://api.stockfighter.io/ob/api"
 
+#Quotes Url
+#url = API_HOST + "/venues/" + venue + "/stocks/" + stock + "/quote"
 
-url = API_HOST + "/venues/" + venue + "/stocks/" + stock + "/quote"
-r = requests.get(url)
-print (r.json())
+#Orders Url
+url = API_HOST + "/venues/" + venue + "/stocks/" + stock + "/orders"
+
+headers = {"X-Starfighter-Authorization" : api_key}
+
+order = {
+    "account" : account,
+    "venue" : venue,
+    "symbol" : stock,
+    "price" : 4000,  #$40.00 -- probably ludicrously high
+    "qty" : 100,
+    "direction" : "buy",
+    "orderType" : "market"  # See the order docs for what a limit order is
+}
+
+order = json.JSONEncoder().encode(order)
+
+r = requests.post(url, headers = headers, data = order)
+print (r.text)
+#f = open('stock_results.json', 'w')
+#f.write(str(r.json()))
+#f.close()
+#print (r.json())
